148552cab7d453dc1898f68f99b07ef9a4608c00
AlexeyPetrochenko <Gyffi-99@yandex.ru>
2024-06-13 23:01:08 +0300

    Создана и инициализированна БД созданна модель News

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from webapp.weather import weather_by_city
 from webapp.python_news import get_news_python
+from webapp.model import db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/')
     def index():
--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,9 @@
+import os
+
+
 WEATHER_DEFAULT_CITY = 'Roslavl, Russia'
 WEATHER_API_KEY = 'fa0e05c81f4148398d0140136243105'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
